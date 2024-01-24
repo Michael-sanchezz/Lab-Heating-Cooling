fae9a4cae181c1b2966b3aa0cfdb18c60ae7bbe3
Michael <michaelsmike7@yahoo.com>
2024-01-24 14:21:51 -0500

    added psuedocode

--- Lab-Heating-Cooling.py
+++ Lab-Heating-Cooling.py
@@ -1,23 +1,36 @@
+#Define a function named heating_cooling that takes in 2 parameters: actual_temp and a desired_temp.
+#Print one of these three things: Run A/C, Run heat, or Standby depending on input.
 def heating_cooling(des, act):
+    print(f"desired temp is {des}, actual temp is {act}" )
     if des>act:
         print("Raise the heat!")
+        print()
     elif des < act:
         print("Run the A/C!")
+        print()
     else:
         print("Standby")
+        print()
 
 des_temp = input("enter desired temp ")
 act_temp = input("enter actual temp ")
 heating_cooling(des_temp, act_temp)
+heating_cooling(22, 50)
 print()
+"""
+Define a function named convert_temp with 2 parameters: temp_celsius (a number)
+and target_unit (a string, either “C”, “F”, or “K”).
+"""
+
 def convert_temp(temp_celsius, target_unit):
+    target_unit=target_unit.upper()
     int(temp_celsius)
-    if target_unit == "k":
+    if target_unit == "K":
         kel=temp_celsius + 273.15
         print(f"kelvin value is: {kel}")
-    elif target_unit == "c":
+    elif target_unit == "C":
         print("value unchanged")
-    elif target_unit == "f":
+    elif target_unit == "F":
         print((temp_celsius*1.8)+32)
     # match target_unit:
     #     case "k":
@@ -27,7 +40,6 @@ def convert_temp(temp_celsius, target_unit):
     #         print("value unchanged")
     #     case "f":
     #         print((temp_celsius * 1.8) + 32)
-
 cel = int(input("enter the celsius value "))
-convert = input("convert celsius to which value c/f/k ".lower())
+convert = input("convert celsius to which value C/F/K ")
 convert_temp(cel,convert)
