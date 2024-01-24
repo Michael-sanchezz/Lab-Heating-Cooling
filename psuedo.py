31b5e6919178442b9526ce0933f7b1e2482a3a72
Michael <michaelsmike7@yahoo.com>
2024-01-24 14:33:51 -0500

    more testing

---
+++ psuedo.py
@@ -0,0 +1,10 @@
+#Define a function named heating_cooling that takes in 2 parameters: actual_temp and a desired_temp.
+#Print one of these three things: Run A/C, Run heat, or Standby depending on input.
+
+"""
+Define a function named convert_temp with 2 parameters: temp_celsius (a number)
+and target_unit (a string, either “C”, “F”, or “K”).
+Write a switch statement that checks the target_unit and returns the temperature converted to that unit.
+Notes: K stands for Kelvin. C requires no conversion, return the original temp.
+
+"""
