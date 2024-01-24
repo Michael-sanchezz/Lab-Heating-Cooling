927605073599e71184ab679796f19a2ef3809a3c
Michael <michaelsmike7@yahoo.com>
2024-01-24 14:31:39 -0500

    testing

--- Lab-Heating-Cooling.py
+++ Lab-Heating-Cooling.py
@@ -1,5 +1,4 @@
-#Define a function named heating_cooling that takes in 2 parameters: actual_temp and a desired_temp.
-#Print one of these three things: Run A/C, Run heat, or Standby depending on input.
+
 def heating_cooling(des, act):
     print(f"desired temp is {des}, actual temp is {act}" )
     if des>act:
@@ -17,13 +16,6 @@ act_temp = input("enter actual temp ")
 heating_cooling(des_temp, act_temp)
 heating_cooling(22, 50)
 print()
-"""
-Define a function named convert_temp with 2 parameters: temp_celsius (a number)
-and target_unit (a string, either “C”, “F”, or “K”).
-Write a switch statement that checks the target_unit and returns the temperature converted to that unit.
-Notes: K stands for Kelvin. C requires no conversion, return the original temp.
-
-"""
 
 def convert_temp(temp_celsius, target_unit):
     target_unit=target_unit.upper()
